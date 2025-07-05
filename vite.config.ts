7d4c77fa2cb9f97146aa11d9566818cae66892aa
Arauz <86898345+kjarj54@users.noreply.github.com>
2025-07-05 16:13:58 -0600

    feat: add build verification step and set NODE_ENV to production in deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/DrawTogether/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+export default defineConfig(({ command }) => {
+  const base = command === 'build' ? '/DrawTogether-FrontEnd/' : '/';
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    base,
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: false,
+    }
   }
 })
