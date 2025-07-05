4e3ad526668d4a8ba6d4f24ad61f6305090f12f3
Arauz <86898345+kjarj54@users.noreply.github.com>
2025-07-05 16:05:12 -0600

    feat: implement GitHub Pages deployment workflow and enhance SPA support

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: process.env.NODE_ENV === 'production' ? '/DrawTogether/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
